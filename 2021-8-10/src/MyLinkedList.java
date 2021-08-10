7f9d023f5cd2758c9bb817a9652cf91456ac9425
sars666 <1491243047@qq.com>
2021-08-10 11:57:00 +0800

    单链表

--- 2021-8-10/src/MyLinkedList.java
+++ 2021-8-10/src/MyLinkedList.java
@@ -38,6 +38,15 @@ public class MyLinkedList {
         System.out.println();
     }
 
+    public void display2(Node newHead){
+
+        Node cur = newHead;
+        while ( cur != null){
+            System.out.print(cur.data+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
     public  void  addLast(int data){
         Node node =new Node(data);
         if(this.head == null){
@@ -125,5 +134,61 @@ public class MyLinkedList {
         cur.next = del.next;
 
     }
+    public boolean contains(int key){
+        Node cur = this.head;
+        while (cur != null){
+            if(cur.data == key){
+                return true;
+            }
+            cur = cur.next;
+        }
+       return false;
+    }
+
+    public void removeAllKey(int key){
+        if(this.head == null)return;
+        Node prev = this.head;
+        Node cur = this.head.next;
+        while(cur != null){
+            if(cur.data == key){
+                prev.next = cur.next;
+                cur = cur.next;
+            }else{
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        if(this.head.data == key){
+            this.head = this.head.next;
+        }
+
+    }
+
+    public Node reverseList(){
+        Node newHead = null;
+        Node cur = this.head;
+        Node prev = null;
+
+        while (cur != null){
+            Node curNext = cur.next;
+            if(curNext == null){
+                newHead = cur;
+            }
+            cur.next = prev;
+            prev = cur;
+            cur = curNext;
+        }
+        return newHead;
+    }
+    public Node middleNode(){
+        Node slow = this.head;
+        Node fast = this.head;
+        while (fast !=null && fast.next !=null){
+            fast =fast.next.next;
+            slow =slow.next;
+        }
+        return slow;
+    }
+
 }
 
